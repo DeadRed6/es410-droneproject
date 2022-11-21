6b23dfbfe0c2452a205ee796d367e3278883da32
Sebastian Gunner <git@sftgunner.com>
2022-11-21 17:12:09 +0000

    Add stitching proof of concept
    + Stitching proof of concept
    + Sample images from Google and video

---
+++ stitching_poc/sftgunner/test.py
@@ -0,0 +1,40 @@
+import cv2
+from time import sleep
+image_paths=['1.png','2.png','3.png']
+# initialized a list of images
+imgs = []
+ 
+for i in range(len(image_paths)):
+    imgs.append(cv2.imread('screenshots/'+image_paths[i]))
+    #imgs[i]=cv2.resize(imgs[i],(0,0),fx=0.4,fy=0.4)
+    # this is optional if your input images isn't too large
+    # you don't need to scale down the image
+    # in my case the input images are of dimensions 3000x1200
+    # and due to this the resultant image won't fit the screen
+    # scaling down the images
+# showing the original pictures
+cv2.imshow('1',imgs[0])
+cv2.waitKey(0)
+cv2.imshow('2',imgs[1])
+cv2.waitKey(0)
+cv2.imshow('3',imgs[2])
+cv2.waitKey(0)
+# cv2.imshow('4',imgs[3])
+# cv2.imshow('5',imgs[4])
+ 
+stitchy=cv2.Stitcher.create()
+(dummy,output)=stitchy.stitch(imgs)
+ 
+if dummy != cv2.STITCHER_OK:
+  # checking if the stitching procedure is successful
+  # .stitch() function returns a true value if stitching is
+  # done successfully
+    raise Exception("stitching ain't successful")
+else:
+    print('Your Panorama is ready!!!')
+ 
+# final output
+cv2.imshow('final result',output)
+ 
+cv2.waitKey(0)
+
