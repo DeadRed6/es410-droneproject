9801b6e66aa880951feb26afff1eb14b39657d4d
Sebastian Gunner <git@sftgunner.com>
2022-11-26 17:29:59 +0000

    Add UAV-Image-Stitching

--- stitching_poc/sftgunner/test.py
+++ stitching_poc/sftgunner/test.py
@@ -1,12 +1,17 @@
 import cv2
 from time import sleep
-image_paths=['1.png','2.png','3.png']
+import glob
+image_paths = glob.glob('dji/*.jpg')
+# image_paths=['1.png','2.png','3.png']
 # initialized a list of images
 imgs = []
+
+limit = 2
+#limit = len(image_paths)
  
-for i in range(len(image_paths)):
-    imgs.append(cv2.imread('screenshots/'+image_paths[i]))
-    #imgs[i]=cv2.resize(imgs[i],(0,0),fx=0.4,fy=0.4)
+for i in range(limit):
+    imgs.append(cv2.imread(image_paths[i]))
+    imgs[i]=cv2.resize(imgs[i],(0,0),fx=0.4,fy=0.4)
     # this is optional if your input images isn't too large
     # you don't need to scale down the image
     # in my case the input images are of dimensions 3000x1200
@@ -15,24 +20,31 @@ for i in range(len(image_paths)):
 # showing the original pictures
 cv2.imshow('1',imgs[0])
 cv2.waitKey(0)
-cv2.imshow('2',imgs[1])
-cv2.waitKey(0)
-cv2.imshow('3',imgs[2])
-cv2.waitKey(0)
+# cv2.imshow('2',imgs[1])
+# cv2.waitKey(0)
+# cv2.imshow('3',imgs[2])
+# cv2.waitKey(0)
 # cv2.imshow('4',imgs[3])
 # cv2.imshow('5',imgs[4])
+
+output = imgs[0]
+
+for i in range(1,limit):
+    images_to_stitch = [output,imgs[i]]
+    print("Initialising stitcher") 
+    stitchy=cv2.Stitcher.create()
+    print("Attempting stitch for images "+str(i-1)+" and "+str(i))
+    (dummy,output)=stitchy.stitch(imgs)
  
-stitchy=cv2.Stitcher.create()
-(dummy,output)=stitchy.stitch(imgs)
+    if dummy != cv2.STITCHER_OK:
+    # checking if the stitching procedure is successful
+    # .stitch() function returns a true value if stitching is
+    # done successfully
+        raise Exception("stitching ain't successful")
+    else:
+        print("Succeeded stitch for images "+str(i-1)+" and "+str(i))
  
-if dummy != cv2.STITCHER_OK:
-  # checking if the stitching procedure is successful
-  # .stitch() function returns a true value if stitching is
-  # done successfully
-    raise Exception("stitching ain't successful")
-else:
-    print('Your Panorama is ready!!!')
- 
+print("Completed all stitches successfully")
 # final output
 cv2.imshow('final result',output)
  
