d9734dc839236473e1f134fe617194790010983a
NitharsanT <117277680+NitharsanT@users.noreply.github.com>
2023-01-18 15:20:01 +0000

    Add files via upload

---
+++ stitching_poc/image_res_increase_2.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import PIL
+img = cv2.imread("DJI_0221.JPG")
+
+hir1 = cv2.pyrUp(img)
+hir2 = cv2.pyrUp(hir1)
+hir3 = cv2.pyrUp(hir2)
+
+cv2.imshow("Original image", img)
+cv2.imshow("First scaled up image", hir1)
+cv2.imshow("Second scaled ip image", hir2)
+cv2.imshow("Third scaled up image", hir3)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows
